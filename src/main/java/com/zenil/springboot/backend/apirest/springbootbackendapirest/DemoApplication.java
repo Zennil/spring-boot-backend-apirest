2cab137a8701cd1430c2680f4906e3fcf54642b4
Armando Zenil <szenil.11@gmail.com>
2020-03-18 16:28:20 -0600

    Commit inicial

---
+++ src/main/java/com/zenil/springboot/backend/apirest/springbootbackendapirest/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.zenil.springboot.backend.apirest.springbootbackendapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zenil/springboot/backend/apirest/springbootbackendapirest/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenil.springboot.backend.apirest.springbootbackendapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
