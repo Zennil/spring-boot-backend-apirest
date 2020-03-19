3f33bbdad41badaa7b6e3e3d4222cd1181d39ae6
Armando Zenil <szenil.11@gmail.com>
2020-03-19 13:27:40 -0600

    Se crean los metodos para Show y Create en el controlador de Cliente

--- src/main/java/com/zenil/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
+++ src/main/java/com/zenil/springboot/backend/apirest/springbootbackendapirest/controllers/ClienteRestController.java
@@ -6,15 +6,20 @@ import com.zenil.springboot.backend.apirest.springbootbackendapirest.models.enti
 import com.zenil.springboot.backend.apirest.springbootbackendapirest.models.services.IClienteService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * ClienteRestController
  */
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
@@ -26,5 +31,18 @@ public class ClienteRestController {
     public List<Cliente> index() {
         return clienteService.findAll();
     }
+
+    @GetMapping(value = "/clientes/{id}")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Cliente show(@PathVariable Long id) {
+        return clienteService.findById(id);
+    }
+
+    @PostMapping(value = "/clientes")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Cliente create(@RequestBody Cliente cliente) {
+        return clienteService.save(cliente);
+    }
+
 
 }
--- src/main/java/com/zenil/springboot/backend/apirest/springbootbackendapirest/models/entity/Cliente.java
+++ src/main/java/com/zenil/springboot/backend/apirest/springbootbackendapirest/models/entity/Cliente.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -38,6 +39,11 @@ public class Cliente implements Serializable {
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)
     private Date createAt;
+
+    @PrePersist
+    public void setCreateDate() {
+        createAt = new Date();
+    }
 
     public Long getId() {
         return id;
