6b0b9454d7c13a0d775a6395adbf2b452e233024
Armando Zenil <szenil.11@gmail.com>
2020-03-26 18:10:25 -0600

    Se crean entidades necesarias para seguridad: Usuario y Role

---
+++ src/main/java/com/zenil/springboot/backend/apirest/springbootbackendapirest/models/entity/Role.java
@@ -0,0 +1,61 @@
+package com.zenil.springboot.backend.apirest.springbootbackendapirest.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ * Role
+ */
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String nombre;
+
+    // No se va a utilizar, solo es para el ejemplo
+    @ManyToMany(mappedBy = "roles")
+    private List<Usuario> usuarios;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+}
